d58968805ca38a5fe951578a97f3ce5d2bcccb5b
eli <eelihalperin@gmail.com>
2026-07-08 17:23:03 +0300

    creating function - readFile, writeFile

---
+++ io.js
@@ -0,0 +1,10 @@
+import fs from "fs/promises";
+
+export async function readFile(path) {
+  const data = await fs.readFile(path, "utf-8");
+  return JSON.parse(data);
+}
+
+export async function writeFile(path, content) {
+  const data = await fs.writeFile(path, content);
+}
