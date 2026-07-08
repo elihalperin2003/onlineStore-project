b500fafc55667dbfb29d4e872cebacceb59ff3e3
eli <eelihalperin@gmail.com>
2026-07-08 17:08:28 +0300

    creating route - /health

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ server.get("/", (req, res) => {
   res.json({ seccuss: true, message: "The server running now" });
 });
 
+server.get("/health", (req, res) => {
+  res.status(200).json({ seccuss: true, message: "status: healthy" });
+});
+
 server.listen(PORT, () => {
   console.log(`listen to port ${PORT}`);
 });
