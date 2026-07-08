70d1c846b218cd9b33cd7b18f1bdca36b86559a6
eli <eelihalperin@gmail.com>
2026-07-08 17:03:21 +0300

    creating route - /

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 import express from "express";
 
 const PORT = process.env.PORT;
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.json({ seccuss: true, message: "The server running now" });
+});
+
+server.listen(PORT, () => {
+  console.log(`listen to port ${PORT}`);
+});
